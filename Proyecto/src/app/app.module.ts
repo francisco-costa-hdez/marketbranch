2bdd2a57da63716cb5c53deadad5d6624165d9ce
Bienve Carbonell <bienvenido98@hotmail.es>
2021-03-27 18:53:48 +0100

    Header y footer introducidos

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+
+const rutas=[
+  { path: 'home', component: HomeComponent},
+  { path: 'favs', component: FooterComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  // { path: '**', component: Pagina404Component}
+] 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
