552cf95fddd1a953b5b1a6d2d2a647e174bfaeed
Bienve Carbonell <bienvenido98@hotmail.es>
2021-04-01 13:31:08 +0200

    Añado componentes: form-product,contact y terms, y modifico customer y form-user

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -20,6 +20,9 @@ import { FAQComponent } from './faq/faq.component';
 import { ReportABugComponent } from './report-abug/report-abug.component';
 import { Error404Component } from './error404/error404.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { TermsComponent } from './terms/terms.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormProductComponent } from './form-product/form-product.component';
 
 
 const rutas=[
@@ -32,6 +35,9 @@ const rutas=[
   { path: 'tienda', component: ShopComponent},
   { path: 'informar', component: ReportABugComponent},
   { path: 'aboutus', component: AboutUsComponent},
+  { path: 'terminos', component: TermsComponent},
+  { path: 'contacto', component: ContactComponent},
+  { path: 'registroprod', component: FormProductComponent},
   // { path: '**', component: FAQComponent}
   { path: '**', component: Error404Component}
 ] 
@@ -55,7 +61,10 @@ const rutas=[
     FAQComponent,
     ReportABugComponent,
     Error404Component,
-    AboutUsComponent
+    AboutUsComponent,
+    TermsComponent,
+    ContactComponent,
+    FormProductComponent
   ],
   imports: [
     BrowserModule,
