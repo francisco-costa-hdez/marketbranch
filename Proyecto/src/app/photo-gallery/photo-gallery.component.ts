d667062d7395b104945f40a330da29939e9e2ef0
Francisco Costa <francisco.costa.hdez@gmail.com>
2021-04-02 19:37:57 +0200

    termino la galeria y retoco cosas

--- Proyecto/src/app/photo-gallery/photo-gallery.component.ts
+++ Proyecto/src/app/photo-gallery/photo-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-photo-gallery',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoGalleryComponent implements OnInit {
 
+  @Output() control = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  hide() {
+    this.control.emit(false);
+  }
 }
