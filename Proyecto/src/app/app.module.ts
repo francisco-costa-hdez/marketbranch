362e9abb00b6fb960d49a63ef36f68ab869ff6bf
Bienve Carbonell <bienvenido98@hotmail.es>
2021-04-03 19:01:29 +0200

    Cambio filtro en shop y añado componente shop-management

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { FormProductComponent } from './form-product/form-product.component';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
 import { FormShopComponent } from './form-shop/form-shop.component';
+import { ShopManagementComponent } from './shop-management/shop-management.component';
 
 const rutas=[
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -42,6 +43,7 @@ const rutas=[
   { path: 'contacto', component: ContactComponent},
   { path: 'registroprod', component: FormProductComponent},
   { path: 'registroshop', component: FormShopComponent},
+  { path: 'manageshop', component: ShopManagementComponent},
   { path: 'faq', component: FAQComponent},
   { path: '**', component: Error404Component}
 ] 
@@ -70,7 +72,8 @@ const rutas=[
     ContactComponent,
     FormProductComponent,
     PhotoGalleryComponent,
-    FormShopComponent
+    FormShopComponent,
+    ShopManagementComponent
   ],
   imports: [
     BrowserModule,
