12bf2189dbe6b83bc07653ef181c75afc4103e20
Francisco Costa <francisco.costa.hdez@gmail.com>
2021-04-07 19:14:37 +0200

    error de merge :(

--- Proyecto/src/app/home/home.component.ts
+++ Proyecto/src/app/home/home.component.ts
@@ -8,8 +8,8 @@ import { MarketPlaceDBService } from 'src/market-place-db.service';
 })
 export class HomeComponent implements OnInit {
 
-  products = [];
   latest=[];
+  best=[];
   shops;
   latestShops=[];
   loading = true;
@@ -25,27 +25,34 @@ export class HomeComponent implements OnInit {
   getProducts() {
     this.db.findAllProducts().subscribe(
       (response) => {
-        this.products = [];
+        let products = [];
         if (response["products"]) {
-          response["products"].forEach((item) => {
-            let newProduct = {
-              id: item.id,
-              name: item.name,
-              price: item.price,
-              discount: item.discount,
-              shop_id: item.shop_id,
-              media_rating: item.media_rating
-            };
-            this.products.push(newProduct);
+          response["products"].forEach((item) =>{
+            let newProduct = item;
+            products.push(newProduct);
           });
           for(let i=0;i<=11;i++){
-            this.latest[i]=this.products[i];
+            this.latest[i]=products[i];
           }
           console.table(this.latest)
+          for(let z=0;z<(products.length-1);z++){
+            for(let j=z+1;j<products.length;j++){
+                if(Number.parseFloat(products[z].price)<Number.parseFloat(products[j].price)){
+                    //Intercambiamos valores
+                    let aux=products[z]
+                    products[z]=products[j]
+                    products[j]=aux
+ 
+                }
+            }
+        }
+        
+        this.best=products
           this.loading = false;
 
+
         }
-        console.table(this.products)
+        console.table(products)
       },
       (error) => {
         console.error('Request failed with error');
