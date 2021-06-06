7b698e83ad403549262eee92f32f9a466d9654c5
Bienve Carbonell <bienvenido98@hotmail.es>
2021-06-06 18:50:03 +0200

    Cambios en contacto , informar de un error y patrones email

--- Proyecto/src/app/contact/contact.component.ts
+++ Proyecto/src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { MarketPlaceDBService } from '../market-place-db.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm:FormGroup;
+
+  constructor(private db: MarketPlaceDBService,private form:FormBuilder) {
+    this.contactForm=this.form.group(
+      {
+        message:['',Validators.required],
+        name:['',Validators.required],
+        email:['',Validators.compose([Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),Validators.required])],
+        
+      }
+    )
+   }
 
   ngOnInit(): void {
+    (function() {
+      'use strict';
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+    })();
   }
 
 }
--- Proyecto/src/app/report-abug/report-abug.component.ts
+++ Proyecto/src/app/report-abug/report-abug.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { MarketPlaceDBService } from '../market-place-db.service';
 
 @Component({
   selector: 'app-report-abug',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportABugComponent implements OnInit {
 
-  constructor() { }
+  bugForm:FormGroup;
+
+  constructor(private db: MarketPlaceDBService,private form:FormBuilder) { 
+
+    this.bugForm=this.form.group(
+      {
+        message:['',Validators.required],
+        points:[''],
+        email:['',Validators.compose([Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),Validators.required])],
+        
+      }
+    )
+  }
 
   ngOnInit(): void {
+    (function() {
+      'use strict';
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+    })();
   }
 
 }
